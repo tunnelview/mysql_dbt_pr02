622c7aa54f9fc89fa271a84282d946917d5c4a5d
tunnelview <abrahamshibin1@gmail.com>
2024-03-03 16:23:23 +1100

    Changed to Jinja template

--- models/Jinja_practice_models/order_count_by_customer.sql
+++ models/Jinja_practice_models/order_count_by_customer.sql
@@ -1,10 +1,13 @@
+
+{{ config(materialized='table') }}
+
 SELECT 
 c.customer_id, 
 c.first_name, 
 c.last_name, 
 COUNT(o.order_id) AS order_count
-FROM stg_customers c
-JOIN stg_orders o 
+FROM  {{ ref('stg_customers')}} c 
+JOIN {{ ref('stg_orders')}} o 
 ON c.customer_id = o.user_id
 GROUP BY 
 c.customer_id, 
