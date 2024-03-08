f84c5377b80c5a3865c43652507d7ccd5cbd4608
tunnelview <abrahamshibin1@gmail.com>
2024-03-08 22:12:00 +1100

    Remove extra comma and implement loop.last as per jinja template

--- models/Jinja_practice_models/jinja_payments_pivot.sql
+++ models/Jinja_practice_models/jinja_payments_pivot.sql
@@ -11,7 +11,7 @@ final as (
        order_id,
        {% for payment_method in payment_methods -%}
  
-       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
        {%- if not loop.last -%}
          ,
        {% endif -%}
