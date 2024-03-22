a78c2478cf2412720f577749f463cf58b0ac18c0
tunnelview <abrahamshibin1@gmail.com>
2024-03-22 21:51:00 +1100

    Update notes to remember one of the place to configure the incremental strategies

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='table') }} -- This is one of the places to configure the incremental strategies.
 
 with source_data as (
 
