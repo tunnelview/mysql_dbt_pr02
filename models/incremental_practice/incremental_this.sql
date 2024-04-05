f188c83da5409c311ba1dda3a1f691bb1a80384d
tunnelview <abrahamshibin1@gmail.com>
2024-04-05 22:23:59 +1100

    Commeted line 3 - config block

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -1,6 +1,6 @@
 --This is the second strategy for incremental
 
-{{ config(materialized='incremental') }}
+--{{ config(materialized='incremental') }}
 /*
 select * from [dbo].[Orders] -- Here 
 where {{this}}.order_datetime > max({{this}}.order_datetime
