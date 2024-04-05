ae32ff9c4cc7e9fa067d26383342c2877c414503
tunnelview <abrahamshibin1@gmail.com>
2024-04-05 21:59:14 +1100

    Uncomment the lines as the previous run created the model in the warehouse

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -1,6 +1,6 @@
 --This is the second strategy for incremental
 
---{{ config(materialized='incremental') }}
+{{ config(materialized='incremental') }}
 
 select * from [dbo].[Orders] -- Here 
---where order_datetime > max(order_datetime) from {{this}}
+where order_datetime > max(order_datetime) from {{this}}
