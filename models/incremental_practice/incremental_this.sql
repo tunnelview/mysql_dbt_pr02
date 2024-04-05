0cc82909c0c7266e83cabf2610df93689be2bdeb
tunnelview <abrahamshibin1@gmail.com>
2024-04-05 22:28:54 +1100

    Refactored the sql code with ssms intellisense, howeve DBT is still throwing an error

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -5,7 +5,9 @@
 select * from [dbo].[Orders] -- Here 
 where {{this}}.order_datetime > max({{this}}.order_datetime
 */
---INSERT INTO [analytics].[incremental_this]
+
+
+INSERT INTO [analytics].[incremental_this]
 SELECT *
 FROM dbo.Orders
-WHERE [dbo].[Orders.order_datetime] > (SELECT MAX(order_datetime) FROM [analytics].[incremental_this])
+WHERE dbo.[Orders].order_datetime > (SELECT MAX(order_datetime) FROM [analytics].[incremental_this])
