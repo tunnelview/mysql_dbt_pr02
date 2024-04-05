f382dacfd14d4b4e77dfa8f6dfd114d05d368e88
tunnelview <abrahamshibin1@gmail.com>
2024-04-05 22:34:44 +1100

    Refactor the ref function code back to original, as when mentioned within ref function, dbt looks for that model within the model stucture

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -9,5 +9,5 @@ where {{this}}.order_datetime > max({{this}}.order_datetime
 
 INSERT INTO analytics.incremental_this
 SELECT *
-FROM {{ref('dbo.Orders')}}
+FROM dbo.Orders
 WHERE orders.order_datetime > (SELECT MAX(order_datetime) FROM analytics.incremental_this)  
