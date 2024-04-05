3ab6a0e075aae0382f1a33287836c8b35071c03d
tunnelview <abrahamshibin1@gmail.com>
2024-04-05 22:26:06 +1100

    Comment Insert into to check if Dbt run works fine, but no, it threw an error

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -1,11 +1,11 @@
 --This is the second strategy for incremental
 
---{{ config(materialized='incremental') }}
+{{ config(materialized='incremental') }}
 /*
 select * from [dbo].[Orders] -- Here 
 where {{this}}.order_datetime > max({{this}}.order_datetime
 */
-INSERT INTO [analytics].[incremental_this]
+--INSERT INTO [analytics].[incremental_this]
 SELECT *
 FROM dbo.Orders
 WHERE [dbo].[Orders.order_datetime] > (SELECT MAX(order_datetime) FROM [analytics].[incremental_this])
