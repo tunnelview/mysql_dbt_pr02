a79ed587e11cbcc0826aa6bb17794340f79b59bb
tunnelview <abrahamshibin1@gmail.com>
2024-04-06 17:17:04 +1100

    Refactor code to be able run dbt model but getting an error due to macro

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -13,12 +13,25 @@ FROM dbo.Orders
 WHERE orders.order_datetime > (SELECT MAX(order_datetime) FROM analytics.incremental_this)  
 
 
-INSERT INTO
+/*
 SELECT 
     *
 FROM 
     dbo.Orders
 {% if is_incremental() %}
 WHERE 
-    LAST_UTC_TS > '{{dbt_utils.get_last_utc_ts(this, 'LAST_UTC_TS')}}' --(select max(LAST_UTC_TS) from {{ this }})
+    orders.order_datetime > '{{dbt_utils.get_last_utc_ts(this, 'orders.order_datetime')}}' --(select max(LAST_UTC_TS) from {{ this }})
 {% endif %}
+*/
+
+/*{{ config(
+    materialized='incremental'
+    , incremental_strategy='append'
+)}}
+
+select *
+from source_table
+{% if is_incremental() %}
+    where extract(order_datetime from order_datetime) in ({{ var('today_and_last_week') | join(',') }})
+{% endif %}
+*/
