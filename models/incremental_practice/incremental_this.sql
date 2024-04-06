280e010c8cacd54b70ea1cabfc18afe1f0ed024b
tunnelview <abrahamshibin1@gmail.com>
2024-04-06 17:32:01 +1100

    Refactor the model name

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -4,8 +4,19 @@
 
 
 
-INSERT INTO analytics.incremental_this
+/*INSERT INTO analytics.incremental_this
 SELECT *
 FROM dbo.Orders
 WHERE orders.order_datetime > (SELECT MAX(order_datetime) FROM analytics.incremental_this)  
+*/
+
+
+SELECT 
+    *
+FROM 
+    dbo.Orders
+{% if is_incremental() %}
+WHERE 
+    orders.order_datetime > '{{dbt_utils.get_last_utc_time(this, 'orders.order_datetime')}}' --(select max(LAST_UTC_TS) from {{ this }})
+{% endif %}
 
