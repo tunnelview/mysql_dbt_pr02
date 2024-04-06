ed7cf2a0d70fd4bbd0ac2b41e872acedfa8e993d
tunnelview <abrahamshibin1@gmail.com>
2024-04-06 16:43:53 +1100

    Implement where clause using this keyword by using dbt utils package as sql server can't read 'this' keyword

--- models/incremental_practice/incremental_this.sql
+++ models/incremental_practice/incremental_this.sql
@@ -11,3 +11,14 @@ INSERT INTO analytics.incremental_this
 SELECT *
 FROM dbo.Orders
 WHERE orders.order_datetime > (SELECT MAX(order_datetime) FROM analytics.incremental_this)  
+
+
+INSERT INTO
+SELECT 
+    *
+FROM 
+    dbo.Orders
+{% if is_incremental() %}
+WHERE 
+    LAST_UTC_TS > '{{dbt_utils.get_last_utc_ts(this, 'LAST_UTC_TS')}}' --(select max(LAST_UTC_TS) from {{ this }})
+{% endif %}
